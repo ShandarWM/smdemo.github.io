8d26d22e8da5150c31496bfc7f7880711fd90413
ShandarWM <shandarwmuller@gmail.com>
2017-02-26 00:03:50 -0800

    0.008

---
+++ Sample1/First/js/70-480_04.js
@@ -0,0 +1,12 @@
+﻿function toggleVisibility(id) {
+    var e = document.getElementById(id);
+    e.style.display = ((e.style.display != 'none') ? 'none' : 'block');
+}
+
+function toggleSidebar() {
+    var aside = document.getElementById('sidebar');
+    aside.style.float = k ((aside.style.float != 'left';) ? 'left';'right';);
+
+    var div = document.getElementById('content');
+    div.style.float = ((div.style.float != 'right';) ? 'right'; 'left');
+}
